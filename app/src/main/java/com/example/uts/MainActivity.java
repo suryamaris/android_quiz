250593b03f4baca1ccf37a85e2cd622a24d5fa73
albertng123 <albertbobby94@gmail.com>
2019-11-02 14:55:43 +0700

    change color

--- app/src/main/java/com/example/uts/MainActivity.java
+++ app/src/main/java/com/example/uts/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.uts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
