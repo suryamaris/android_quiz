2def9fbb08bae9a73cc3ee081339f11202f560f4
albertng123 <albertbobby94@gmail.com>
2019-11-02 15:14:31 +0700

    change color

--- app/src/main/java/com/example/uts/Multiple_Choice.java
+++ app/src/main/java/com/example/uts/Multiple_Choice.java
@@ -1,4 +1,14 @@
 package com.example.uts;
 
-public class Multiple_Choice {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Multiple_Choice extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.multiple_choice);
+
+    }
 }
