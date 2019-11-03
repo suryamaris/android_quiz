9af2d12d0b042819ebf3c7b37a17afc8b3360f2a
fadhliibrahims <fadhli1999siregar@gmail.com>
2019-11-03 18:23:39 +0700

    Membuat dan Menginisialisasi textview(question) dan button(ans_a, ans_b, ans_c, ans_d) berdasarkan layout multiple_choice

--- app/src/main/java/com/example/uts/Multiple_Choice.java
+++ app/src/main/java/com/example/uts/Multiple_Choice.java
@@ -1,13 +1,23 @@
 package com.example.uts;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Multiple_Choice extends AppCompatActivity {
+    private TextView question;
+    private Button ansA, ansB, ansC, ansD;
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multiple_choice);
+        question = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_a);
+        ansB = findViewById(R.id.ans_b);
+        ansC = findViewById(R.id.ans_c);
+        ansD = findViewById(R.id.ans_d);
     }
 }
